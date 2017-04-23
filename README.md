5a57b3954dbe1541ce4b143aad31df27ba201dd2
albno273 <albireo0725.mikumiku@gmail.com>
2017-04-24 00:13:24 +0900

    Rewrite in async/await

--- README.md
+++ README.md
@@ -1,43 +1,27 @@
 # grkBlogBot / Check GRKB Blog Update
 
-## これなに
-気付いたら先代ごらくブログbot ([@gorakublog](https://twitter.com/gorakublog)) が1年半前に死んでいたので作りました。
+## About
+気付いたら先代ごらくブログbot ([@gorakublog](https://twitter.com/gorakublog)) が動作を停止していたので作りました。
+
 更新を検知するとタイトルとURLが ごらくブログbot v2 ([@grk_b_log_bot](https://twitter.com/grk_b_log_bot)) からツイートされます。
+
 更新確認は3分毎です。
 
-## つかいかた
+## Usage
 1. [@grk_b_log_bot](https://twitter.com/grk_b_log_bot) をフォロー
 1. ツイートが来たらアクセスする
 1. 便利
 
-## 使ってるもの
+## Dependencies
+- node.js (- v7.4.0)
+    - <s>[cheerio-httpcli](https://www.npmjs.com/package/cheerio-httpcli)</s>
+    アーリーウィングブログにも RSS があったのでそっちを使うようにしました。Thx: [@dolciss](https://twitter.com/L_tan/status/826436855105097728)
+    - [twitter](https://www.npmjs.com/package/twitter)
+    - [confu](https://www.npmjs.com/package/confu)
+    - [feedparser](https://www.npmjs.com/package/feedparser)
+    - [request](https://www.npmjs.com/package/request)
+    - [cron](https://github.com/kelektiv/node-cron)
+    - [pm2](https://www.npmjs.com/package/pm2)
 
-node.js (v7.4.0 で動作します)
-<s>
-- [cheerio-httpcli](https://www.npmjs.com/package/cheerio-httpcli)
-アーリーウィング公式ブログの HTML スクレイピングに使用 <font color="Gray"><s> RSS フィードつけてくれ</s></font>
-</s>
-アーリーウィングブログにも RSS があったのでそっちを使うようにしました。
-Thx: [@dolciss](https://twitter.com/L_tan/status/826436855105097728)
-- [twitter](https://www.npmjs.com/package/twitter)
-ツイート用
-- [confu](https://www.npmjs.com/package/confu)
-bot の CK/CS を読み込むのに使用
-- [feedparser](https://www.npmjs.com/package/feedparser)
-RSS フィードの読み込みに使用
-- [request](https://www.npmjs.com/package/request)
-HTTP リクエストを送るやつ
-- [cron](https://github.com/kelektiv/node-cron)
-定期実行用
-- [pm2](https://www.npmjs.com/package/pm2)
-実行プロセスのデーモン化に使用
-
-AWS EC2
-- 自前のインスタンスで動いてます
-
-## 問題点
-- ちゃんと動くんですか
-たぶん
-
-## なにかあったら
+## Contact
 [@albNo273](https://twitter.com/albNo273) までご連絡ください。
--- chk_update.js
+++ chk_update.js
@@ -1,14 +1,14 @@
+'use strict';
 /* ごらくブログbotリバイバル */
 
-const twitter = require('twitter'),
-      confu = require('confu'),
-      fs = require('fs'),
+const twitter    = require('twitter'),
+      confu      = require('confu'),
+      fs         = require('fs'),
       FeedParser = require('feedparser'),
-      request = require('request');
+      request    = require('request');
 
 // bot の CK/CS 読み込み
 const conf = confu('.', 'config', 'key.json');
-// console.log(conf);
 
 // token 設定
 const bot = new twitter({
@@ -19,140 +19,58 @@ const bot = new twitter({
 });
 
 // RSS の URL
-const shiori_rss = 'http://feedblog.ameba.jp/rss/ameblo/mikami-shiori/rss20.xml',
-      yuka_rss = 'http://www.earlywing.co.jp/category/blog/feed/',
-      minami_rss = 'http://feedblog.ameba.jp/rss/ameblo/00dpd/rss20.xml',
-      rumi_rss = 'http://feedblog.ameba.jp/rss/ameblo/rumiokubo/rss20.xml';
+const shioriRss = 'http://feedblog.ameba.jp/rss/ameblo/mikami-shiori/rss20.xml',
+      yukaRss   = 'http://www.earlywing.co.jp/category/blog/feed/',
+      minamiRss = 'http://feedblog.ameba.jp/rss/ameblo/00dpd/rss20.xml',
+      rumiRss   = 'http://feedblog.ameba.jp/rss/ameblo/rumiokubo/rss20.xml';
 
-// 確認時のタイトルと URL
-let shiori, yuka, minami, rumi;
+exports.func = async () => {
+    const text = await fs.readFileSync('b_log.txt', { encoding: "utf8" });
+    /**
+    * 確認前の最新の記事タイトルを格納
+    * [0]: 三上枝織   shiori
+    * [1]: 大坪由佳     yuka
+    * [2]: 津田美波   minami
+    * [3]: 大久保瑠美   rumi
+    */
+    const titleArr = await text.replace(/\r/g, "").split("\n");
+    let flg = false;
 
-exports.func = function () {
+    const shioriRes = await checkRSS(shioriRss);
+    console.log('shiori-recent: ' + titleArr[0]);
+    console.log('shiori-result: ' + shioriRes[0]);
+    if (titleArr[0] != shioriRes[0]) {
+        tweetUpdate('三上枝織', shioriRes);
+        flg = true;
+    }
 
-    Promise.resolve()
-        // 1. 直近の記事タイトルを読み込む
-        .then(function () {
-            return new Promise(function (resolve, reject) {
-                const text = fs.readFileSync('b_log.txt', { encoding: "utf8" });
-                /**
-                 * 確認前の最新の記事タイトルを格納
-                 * [0]: 三上枝織   shiori
-                 * [1]: 大坪由佳     yuka
-                 * [2]: 津田美波   minami
-                 * [3]: 大久保瑠美   rumi
-                 */
-                const title_arr = text.replace(/\r/g, "").split("\n");
-                console.log('Recent titles loaded.');
+    const yukaRes = await checkRSS(yukaRss);
+    console.log('yuka-recent:   ' + titleArr[1]);
+    console.log('yuka-result:   ' + yukaRes[0]);
+    if (titleArr[1] != yukaRes[0]) {
+        tweetUpdate('大坪由佳', yukaRes);
+        flg = true;
+    }
 
-                resolve(title_arr);
-            });
-        })
-        // 2. RSSフィードを確認
-        .then(function (title_arr) {
-            return new Promise(function (resolve, reject) {
-                Promise.all([
-                    new Promise(function (resolve, reject) {
-                        checkRSS(shiori_rss, function (err, result) {
-                            if (!err) {
-                                shiori = result;
-                                // console.log(shiori);
-                                console.log('shiori-recent: ' + title_arr[0]);
-                                console.log('shiori-result: ' + shiori[0]);
+    const minamiRes = await checkRSS(minamiRss);
+    console.log('minami-recent: ' + titleArr[2]);
+    console.log('minami-result: ' + minamiRes[0]);
+    if (titleArr[2] != minamiRes[0]) {
+        tweetUpdate('津田美波', minamiRes);
+        flg = true;
+    }
 
-                                if (title_arr[0] != shiori[0]) {
-                                    tweetUpdate('三上枝織', shiori);
-                                    resolve(true);
-                                } else
-                                    resolve(false);
-                            }
-                        });
-                    }),
-                    new Promise(function (resolve, reject) {
-                        checkRSS(yuka_rss, function (err, result) {
-                            if (!err) {
-                                yuka = result;
-                                // console.log(yuka);
-                                console.log('yuka-recent:   ' + title_arr[1]);
-                                console.log('yuka-result:   ' + yuka[0]);
+    const rumiRes = await checkRSS(rumiRss);
+    console.log('rumi-recent:   ' + titleArr[3]);
+    console.log('rumi-result:   ' + rumiRes[0]);
+    if (titleArr[3] != rumiRes[0]) {
+        tweetUpdate('大久保瑠美', rumiRes);
+        flg = true;
+    }
 
-                                if (title_arr[1] != yuka[0] && yuka[0].includes('大坪')) {
-                                    tweetUpdate('大坪由佳', yuka);
-                                    resolve(true);
-                                } else
-                                    resolve(false);
-                            }
-                        });
-                    }),
-                    new Promise(function (resolve, reject) {
-                        checkRSS(minami_rss, function (err, result) {
-                            if (!err) {
-                                minami = result;
-                                // console.log(minami);
-                                console.log('minami-recent: ' + title_arr[2]);
-                                console.log('minami-result: ' + minami[0]);
-
-                                if (title_arr[2] != minami[0]) {
-                                    tweetUpdate('津田美波', minami);
-                                    resolve(true);
-                                } else
-                                    resolve(false);
-                            }
-                        });
-                    }),
-                    new Promise(function (resolve, reject) {
-                        checkRSS(rumi_rss, function (err, result) {
-                            if (!err) {
-                                rumi = result;
-                                // console.log(rumi);
-                                console.log('rumi-recent:   ' + title_arr[3]);
-                                console.log('rumi-result:   ' + rumi[0]);
-
-                                if (title_arr[3] != rumi[0]) {
-                                    tweetUpdate('大久保瑠美', rumi);
-                                    resolve(true);
-                                } else
-                                    resolve(false);
-                            }
-                        });
-                    })
-                ]).then(
-                    function (results) {
-                        // console.log(results);
-                        if (results.indexOf(true) >= 0)
-                            resolve(true);
-                        else
-                            resolve(false);
-                    });
-            })
-        })
-        // 3. 記事タイトルログの更新
-        .then(function (flg) {
-            return new Promise(function (resolve, reject) {
-                if (flg) {
-                    const text = shiori[0] + '\n' + yuka[0] + '\n'
-                            + minami[0] + '\n' + rumi[0];
-                    fs.writeFileSync('b_log.txt', text, function (err) {
-                        if (!err)
-                            console.log('Log update succeeded.');
-                        else {
-                            console.log('Log update failed.');
-                            console.log(err);
-                        }
-                    });
-                    resolve(null);
-                } else {
-                    console.log('Log is not updated.');
-                    resolve(null);
-                }
-            });
-        })
-        .then(function (value) {
-            console.log('All processing is done.');
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-
+    if(flg) {
+        await updateLog(shioriRes[0], yukaRes[0], minamiRes[0], rumiRes[0]);
+    }
 }
 
 /**
@@ -161,61 +79,70 @@ exports.func = function () {
  * @param data 記事タイトルとリンク
  */
 function tweetUpdate(head, data) {
-
-    const tweet_body = '【ブログ更新】' + head + ': ' + data[0] + '\n' + data[1] + '#yuruyuri';
+    const tweet_body = '【ブログ更新】' + head + ': ' + data[0] +
+                       '\n' + data[1] + ' #yuruyuri';
     console.log(tweet_body);
-
     bot.post(
-        'statuses/update',
-        { status: tweet_body },
-        function (err, tweet, response) {
-            if (!err) {
-                console.log('Tweet succeeded.');
-            } else {
-                console.log('Tweet failed.');
-                console.log(err);
-            }
+        'statuses/update', { status: tweet_body }, (err, tweet, response) => {
+            if (!err) console.log('Tweet succeeded.');
+            else      console.log('Tweet failed:', err);
         }
     );
-
 }
 
 /**
  * RSSフィードの確認
- * @param url       RSS の URL
- * @param callback  array - [0]: 記事タイトル [1]: リンク
+ * @param  url     RSS の URL
  */
-function checkRSS(url, callback) {
+function checkRSS(url) {
+    return new Promise((resolve, reject) => {
+        const fp_req      = request(url),
+              feedparser  = new FeedParser();
+        let items = [];
 
-    const fp_req = request(url),
-          feedparser = new FeedParser();
-    let result,
-        items = [];
+        fp_req.on('error', error => {
+            reject(error);
+        });
 
-    fp_req.on('error', function (error) {
-        // Request error
+        fp_req.on('response', function (res) {
+            let stream = this; // TODO: rewrite 'this' in arrow function
+            if (res.statusCode != 200)
+                reject(this.emit('error', new Error('Bad status code')));
+            stream.pipe(feedparser);
+        });
+
+        feedparser.on('error', error => {
+            reject(error);
+        });
+
+        feedparser.on('readable', function () {
+            const item = this.read(); // TODO: rewrite 'this' in arrow function
+            items.push(item);
+        });
+
+        feedparser.on('end', () => {
+            resolve([items[0].title, items[0].link]);
+        });
+    })
+    .catch(err => {
+        console.log('checkRSS error:', err);
+        return err;
     });
+}
 
-    fp_req.on('response', function (res) {
-        let stream = this;
-        if (res.statusCode != 200) {
-            return this.emit('error', new Error('Bad status code'));
-        }
-        stream.pipe(feedparser);
-    });
-
-    feedparser.on('error', function (error) {
-        // Error
-    });
-
-    feedparser.on('readable', function () {
-        item = this.read();
-        items.push(item);
-    });
-
-    feedparser.on('end', function () {
-        result = Array(items[0].title, items[0].link);
-        // console.log(result);
-        callback(null, result);
+/**
+ * ログファイルのアップデート
+ */
+function updateLog(shiori, yuka, minami, rumi) {
+    return new Promise((resolve, reject) => {
+        const text = shiori + '\n' + yuka + '\n' + minami + '\n' + rumi;
+        fs.writeFileSync('b_log.txt', text, err => {
+            if (!err) resolve();
+            else      reject(err);
+        });
+    })
+    .catch(err => {
+        console.log('updateLog error:', err);
+        return err;
     });
 }
